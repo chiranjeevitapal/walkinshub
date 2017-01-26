61e7497b73cd1a29bd03b84bdb4f8b2883e6d454
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2017-01-26 11:13:35 +0530

    fixed bugs

--- src/app/home/postjob.component.ts
+++ src/app/home/postjob.component.ts
@@ -1,7 +1,7 @@
 import { Component  } from '@angular/core';
 
 @Component({
-    template: '<h3>Please send job details to walkinshubadmin@gmail.com </h3>'
+    template: '<h3>Please send job details to walkinshubindia@gmail.com </h3>'
 })
 export class PostJobComponent {
 }
